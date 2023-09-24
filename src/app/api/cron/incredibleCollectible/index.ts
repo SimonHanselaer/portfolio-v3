79ef2b032ff7e22edec5dcb36ba5f8dec16d9621
Simon  Hanselaer <simon.hanselaer@hotmail.com>
2023-09-24 16:26:16 +0200

    :zap: move cron

--- src/app/api/cron/incredibleCollectible/index.ts
+++ src/app/api/cron/incredibleCollectible/index.ts
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const mysql = require("mysql2");
+const { NextResponse } = require("next/server");
 
 export default function insertVote() {
   const connection = mysql.createConnection(process.env.DATABASE_URL as string);
-  console.log("Connected to PlanetScale!");
   connection.query("INSERT INTO ic_votes (id_items, date) VALUES (?, ?)", [
     Math.ceil(Math.random() * 7),
     new Date().toISOString().slice(0, 19).replace("T", " "),
   ]);
   connection.end();
+
+  return NextResponse.json({}, { status: 200 });
 }
