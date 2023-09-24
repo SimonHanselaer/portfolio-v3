c51745d4ab25fd84459f6a9af2ff9fc6aa8e7fda
Simon  Hanselaer <simon.hanselaer@hotmail.com>
2023-09-24 16:17:24 +0200

    :zap: fix env

--- api/cron/incredibleCollectible/index.ts
+++ api/cron/incredibleCollectible/index.ts
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const mysql = require("mysql2");
-const connection = mysql.createConnection(process.env.DATABASE_URL);
-console.log("Connected to PlanetScale!");
-connection.query("INSERT INTO ic_votes (id_items, date) VALUES (?, ?)", [
-  Math.ceil(Math.random() * 7),
-  new Date().toISOString().slice(0, 19).replace("T", " "),
-]);
-connection.end();
+
+export default function insertVote() {
+  const connection = mysql.createConnection(process.env.DATABASE_URL as string);
+  console.log("Connected to PlanetScale!");
+  connection.query("INSERT INTO ic_votes (id_items, date) VALUES (?, ?)", [
+    Math.ceil(Math.random() * 7),
+    new Date().toISOString().slice(0, 19).replace("T", " "),
+  ]);
+  connection.end();
+}
