0b1bd73ebc9419be13cd47b9e2bd780edca77de9
Simon  Hanselaer <simon.hanselaer@hotmail.com>
2023-09-30 13:42:38 +0200

    :zap: update

--- src/app/api/cron/incredibleCollectible/insertVote.ts
+++ src/app/api/cron/incredibleCollectible/insertVote.ts
@@ -4,11 +4,6 @@ import { NextRequest, NextResponse } from "next/server";
 export const runtime = "edge";
 
 export default function insertVote(req: NextRequest) {
-  const cron = req.nextUrl.pathname.split("/")[3];
-  console.log(cron);
-
-  if (!cron) return new Response("no cron provided", { status: 400 });
-
   const connection = mysql.createConnection(
     'mysql://rz5xvipzr5vvv5bmkcgb:pscale_pw_bJZv7MCw5C6QntvUAjRRaPyneqG3ZUYJAbEnDwU0b0x@aws.connect.psdb.cloud/incrediblecollectible?ssl={"rejectUnauthorized":true}'
   );
